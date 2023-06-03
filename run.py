a8f9e470e674fd42e3918f75346a97e0c00d875a
Rob Killick <robkillick@me.com>
2023-06-03 20:42:57 +0000

    feat: create get_coffee_type function to pull coffee sheet data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +13,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('the_coffee_run')
 
-menu = SHEET.worksheet('menu')
+def get_coffee_type():
+    """
+    Pulls data from the sheet "coffee" and assigns to variable "coffee_data".
+    Assigns index 0 of the list "coffee_data" to the variable "header_names.
+    Assigns remaining indexes of list to the variable menu_content  
+    """
+    coffee_menu = SHEET.worksheet("coffee")
+    coffee_data = coffee_menu.get_all_values()
 
-data = menu.get_all_values()
+    # Set first row of 'coffee' sheet as header names
+    header_names = coffee_data[0]
 
-print(data)
+    # Assign rows from coffe_data to variable menu_content 
+    menu_content = coffee_data[-4:]
+    print(header_names)
+    print(menu_content)
+
+get_coffee_type()
