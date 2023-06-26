84fd964d14f4455506f02403d73f61bc621041f2
Rob Killick <robkillick@me.com>
2023-06-26 12:24:09 +0000

    style: add carriage return symbol in quit_app print statement

--- run.py
+++ run.py
@@ -965,7 +965,7 @@ def quit_app():
     os.system('cls' if os.name == 'nt' else 'clear')
     print(
         colored("Thanks for stopping by, see you again soon!\n", 'cyan')
-        )
+        , end="\r")
     title_screen()
 
 
